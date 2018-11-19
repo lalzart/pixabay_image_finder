5e453b5094133f94cafa8bbb774478f29ab9f043
lalzart <lalazart@gmail.com>
2018-11-19 22:01:24 +0900

    added axios

---
+++ src/component/image-results/ImageResults.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types';
+import { GridList, GridTile } from 'material-ui/GridList';
+
+ class ImageResults extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+ImageResults.PropTypes = {
+  images: PropTypes.array.isRequired
+}
+export default ImageResults;
